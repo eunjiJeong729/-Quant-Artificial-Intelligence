a4977cd54637aaddcfe5ab2afc8e0bee81f151ee
eunji0729 <eunji0729@hanyang.ac.kr>
2020-12-11 00:10:38 +0900

    add pandas date

--- date.py
+++ date.py
@@ -1,4 +1,5 @@
 import datetime
+import pandas as pd
 
 # strptime str to datetime
 format = '%Y-%m-%d %H:%M:%S'
@@ -13,3 +14,18 @@ print(type(date_str))
 date_str2 = date_dt.strftime('%Y-%m-%d %H')
 print(date_str2)
 
+# date time use pandas
+# Timestamp 현재까지의 시간 계산
+pd.Timestamp(1239.1238934, unit = 'D')
+untill_sysdate = pd.Timestamp('2019-1-1')
+pd.to_datetime('2019-1-1 12')
+pd.to_datetime(['2018-1-1', '2019-1-2'])
+# date_range 자동 날짜 생성
+date_li = pd.date_range('2020-01', '2020-02', freq='B')
+print(date_li)
+# Time spans 기간 계산
+pd.Period('2020-01')
+# freq로 기간 조정
+pd.Period('2020-02', freq = 'D')
+date_li = pd.period_range('2020-01', '2020-02', freq='D')
+print(date_li)
