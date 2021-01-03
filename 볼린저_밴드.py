69423346be1791c55274aade14f15a601ac3878e
eunji0729 <eunji0729@hanyang.ac.kr>
2021-01-03 14:50:16 +0900

    add return

--- 볼린저_밴드.py
+++ 볼린저_밴드.py
@@ -37,7 +37,7 @@ sample = bollinger.loc[base_data:]
 print(sample.head())
 
 # 진입 또는 청산 신호 발생 시 기록(거래장부) 생성
-book = sample[['Adj Close']].copy()
+book = sample[['Date', 'Adj Close']].copy()
 book['trade'] = ''
 
 # 거래전략
@@ -63,3 +63,36 @@ def tradings(sample,book):
 
 book = tradings(sample, book)
 print(book.tail(10))
+book = book.set_index('Date')
+
+# 전략수익률
+
+def rate_returns(book) :
+    rtn = 1.0
+    book['return'] = 1
+    buy = 0.0
+    sell = 0.0
+    for i in book.index:
+        # long 진입(매수 신호가 있을 경우)
+        if book.loc[i, 'trade'] == 'buy' and book.shift(1).loc[i, 'trade'] == '':
+            buy = book.loc[i, 'Adj Close']
+            print('진입일 : ', i, 'long 진입가격 : ', buy)
+        # long 청산(매도 신호가 있을 경우)
+        elif book.loc[i, 'trade'] == '' and book.shift(1).loc[i, 'trade'] == 'buy':
+            sell = book.loc[i, 'Adj Close']
+            rtn = (sell - buy) / buy + 1 # 손익계산
+            book.loc[i, 'return'] = rtn # 수익률 저장
+            print('청산일 : ', i, 'long 진입가격 : ', buy, ' | long 청산가격 : ', sell, ' | return : ', round(rtn, 4))
+
+        if book.loc[i, 'trade'] == '': # 제로
+            buy = 0.0
+            sell = 0.0
+    acc_rtn = 1.0
+    for i in book.index:
+        rtn = book.loc[i, 'return']
+        acc_rtn = acc_rtn * rtn # 누적 수익률
+        book.loc[i, 'acc return'] = acc_rtn
+    print('Accunulated return : ', round(acc_rtn, 4))
+    return round(acc_rtn, 4)
+
+print(rate_returns(book))
