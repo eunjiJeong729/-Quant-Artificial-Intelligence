8610fc20ef8aa23018eac09ab5a7507c5a1fea94
eunji0729 <eunji0729@hanyang.ac.kr>
2021-02-09 19:50:57 +0900

    add relative_strength_index

--- ETFs_활용_주가방향예측.py
+++ ETFs_활용_주가방향예측.py
@@ -21,3 +21,20 @@ from sklearn.metrics import mean_absolute_percentage_error, r2_score
 from sklearn.metrics import accuracy_score
 from sklearn import svm
 import seaborn as sns; sns.set()
+
+df = pd.read_csv('ETFs_main.csv')
+
+# 기술 지표
+def moving_average(df, n):
+    MA = pd.Series(df['CLOSE_SPY'].rolling(n, min_periods=n).mean(), name='MA_' + str(n))
+    df = df.join(MA)
+    return df
+def volume_moving_average(df, n):
+    VMA = pd.Series(df['VOLUME'].rolling(n, min_periods=n).mean(), name='VMA_' + str(n))
+    df = df.join(VMA)
+    return df
+
+def relative_strength_index(df, n):
+    i = 0
+    UpI = [0]
+    DoI = [0]
